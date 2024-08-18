948101c2143675c21d3f887ce70913f63fd931cb
Mark Lipovetsky <marklipovetsky@Marks-MBP-2.lan>
2024-08-18 18:26:22 +0300

    input validations

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This tool fetches and analyzes data from the CTFd GitHub repository and generates a graph representation of the commit history.
 
+## Python version
+
+Used python 3.12
+
 ## Installation
 
 Clone the repository and install the package:
--- ctfd_analysys/cli.py
+++ ctfd_analysys/cli.py
@@ -1,38 +1,27 @@
 import argparse
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description="Used to analyze CTFd GitHub repository")
+    parser = argparse.ArgumentParser(
+        description="Used to analyze CTFd GitHub repository"
+    )
 
     parser.add_argument(
-        "--log-to", 
+        "--log-to",
         choices=["stdout", "file"],
         default="stdout",
-        help="Specify logging output: stdout or file"
+        help="Specify logging output: stdout or file",
+    )
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+    parser.add_argument("--token", required=True, help="GitHub access token", type=str)
+    parser.add_argument(
+        "--owner", required=True, help="GitHub repository owner", type=str
     )
     parser.add_argument(
-        "--debug", 
-        action="store_true",
-        help="Enable debug mode"
+        "--repo", required=True, help="GitHub repository name", type=str
     )
     parser.add_argument(
-        "--token", 
-        required=True,
-        help="GitHub access token"
-    )
-    parser.add_argument(
-        "--owner",
-        required=True,
-        help="GitHub repository owner"
-    )
-    parser.add_argument(
-        "--repo",
-        required=True,
-        help="GitHub repository name"
-    )
-    parser.add_argument(
-        "--branch",
-        default="master",
-        help="GitHub repository branch"
+        "--branch", default="master", help="GitHub repository branch", type=str
     )
 
     return parser.parse_args()
